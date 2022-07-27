1db44cc68e1ea09a1fbfdafb2697feb6bad6d956
1ierro1ast <lierro00@gmail.com>
2022-07-27 17:28:16 +0300

    update extensions

--- Assets/CodeBase/Extensions/Vector3Extension.cs
+++ Assets/CodeBase/Extensions/Vector3Extension.cs
@@ -13,5 +13,48 @@ namespace CodeBase.Extensions
         {
             return SqrDistance(from, to) < borderAmount;
         }
+
+        public static void ConvertToVector2ZAsY(this Vector3 vector3, out Vector2 to)
+        {
+            to = new Vector2(vector3.x, vector3.z);
+        }
+
+        public static Vector2 ConvertToVector2ZAsY(this Vector3 vector3)
+        {
+            return new Vector2(vector3.x, vector3.z);
+        }
+    }
+
+    public static class Vector2Extension
+    {
+        public static float SqrDistance(this Vector2 from, Vector2 to)
+        {
+            return (to - from).sqrMagnitude;
+        }
+
+        public static bool SqrCompareDistance(this Vector2 from, Vector2 to, float borderAmount)
+        {
+            return SqrDistance(from, to) < borderAmount;
+        }
+
+        public static void ConvertToVector3YAsZ(this Vector2 vector2, out Vector3 to)
+        {
+            to = new Vector3(vector2.x, 0, vector2.y);
+        }
+
+        public static Vector3 ConvertToVector3YAsZ(this Vector2 vector2)
+        {
+            return new Vector3(vector2.x, 0, vector2.y);
+        }
+
+        public static void ConvertToVector3YAsZ(this Vector2 vector2, out Vector3 to, float y)
+        {
+            to = new Vector3(vector2.x, y, vector2.y);
+        }
+
+        public static Vector3 ConvertToVector3YAsZ(this Vector2 vector2, float y)
+        {
+            return new Vector3(vector2.x, y, vector2.y);
+        }
     }
 }
